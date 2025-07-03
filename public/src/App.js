edd3575d8ee216dc709d952605423f55a37cec8a
jsm-Investika <jjsm3826@gmail.com>
2025-07-03 18:45:05 +0300

    Create App.js
    Add App.js inside src

---
+++ public/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './styles.css';
+
+function App() {
+  return (
+    <div className="container">
+      <h1>Karibu INVESTIKA</h1>
+      <p>Jukwaa la kuweka na kuwekeza — Anza safari yako sasa!</p>
+    </div>
+  );
+}
+
+export default App
